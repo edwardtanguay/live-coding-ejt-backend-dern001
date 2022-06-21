b0cf567993a4fe5d4f0d157752a9717f5d5990b5
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-21 10:03:12 +0200

    showing all api urls

--- server.js
+++ server.js
@@ -3,13 +3,32 @@ import { siteData } from './src/models.js';
 
 const app = express();
 const port = process.env.PORT || 3007;
+const fullUrl = `http://localhost:${port}`;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send(siteData);
+	res.send(`
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Info API</title>
+   </head>
+   <body>
+   <h1>Info API</h1> 
+   <ul>
+   ${Object.keys(siteData).map((key) => {
+		return `<li><a href="${fullUrl}/${key}">${fullUrl}/${key}</a></li>`;
+   }).join('')}
+   </ul>
+   </body>
+   </html> 
+    `);
 });
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+	console.log(`Listening at http://localhost:${port}`);
 });
