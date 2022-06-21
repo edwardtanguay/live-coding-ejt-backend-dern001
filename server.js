e2c255c1f1b923f1e9127d8d12aa6c802315ab0e
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-21 11:09:40 +0200

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { siteData } from './src/models.js';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3007;
 const fullUrl = `http://localhost:${port}`;
 
 app.use(express.static('public'));
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send(`
@@ -27,22 +29,29 @@ app.get('/', (req, res) => {
    <body>
    <h1>Info API</h1> 
    <ul>
-   ${Object.keys(siteData).map((key) => {
-		return `<li><a href="${fullUrl}/${key}">${fullUrl}/${key}</a></li>`;
-   }).join('')}
+   <li><a href="${fullUrl}/all">${fullUrl}/all</a></li>
+   ${Object.keys(siteData)
+		.map((key) => {
+			return `<li><a href="${fullUrl}/${key}">${fullUrl}/${key}</a></li>`;
+		})
+		.join('')}
    </ul>
    </body>
    </html> 
     `);
 });
 
-Object.entries(siteData).forEach(entry => {
-    const key = entry[0];
-    const value = entry[1];
-    app.get(`/${key}`, (req, res) => {
-        res.send(value);
-    });
-})
+Object.entries(siteData).forEach((entry) => {
+	const key = entry[0];
+	const value = entry[1];
+	app.get(`/${key}`, (req, res) => {
+		res.send(value);
+	});
+});
+
+app.get(`/all`, (req, res) => {
+	res.send(siteData);
+});
 
 app.listen(port, () => {
 	console.log(`Listening at http://localhost:${port}`);
